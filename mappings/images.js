3a3c42bf0bda77f0772a29906881354834649328
Chatiana <[thatiana.cm@gmail.com]>
2020-04-14 15:03:33 +0100

    changes in app.js, routes/producs, views/products, mappings

---
+++ mappings/images.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'chocolate_cake': 'choco_cake',
+  'fruit-menrengue_cupcakes': 'fruit-merenge_cupcake',
+  'chocolate_chip_cookies': 'cookies_product',
+  'rainbow_cupcakes': 'cupcakes_product',
+  'oatmeal_cookies': 'cookies_product'
+};
