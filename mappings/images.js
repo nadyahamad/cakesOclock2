4ef58cc015cc862d927f7eebade6ba9541fbd24a
Chatiana <[thatiana.cm@gmail.com]>
2020-04-18 13:10:10 +0100

    styles, images

--- mappings/images.js
+++ mappings/images.js
@@ -2,6 +2,6 @@ module.exports = {
   'chocolate_cake': 'choco_cake',
   'fruit-menrengue_cupcakes': 'fruit-merenge_cupcake',
   'chocolate_chip_cookies': 'cookies_product',
-  'rainbow_cupcakes': 'cupcakes_product',
-  'oatmeal_cookies': 'cookies_product'
+  'rainbow_cupcakes': 'rainbow_cupcakes',
+  'oatmeal_cookies': 'oatmeal'
 };
