90991872cbd756d133e812d59fa8b5f5f138e3d8
Chatiana <[thatiana.cm@gmail.com]>
2020-04-26 01:48:25 +0100

    admin

--- routes/adminproducts.js
+++ routes/adminproducts.js
@@ -1,4 +1,8 @@
-  
+/*
+ * GET admin products listing.
+ */ 
+
+ // Connect Users database
   exports.list = function(req, res){
 
     req.getConnection(function(err,connection){
@@ -16,82 +20,65 @@
 
     });
 };
-
+//Add new Product
 exports.add = function(req, res){
     res.render('add_admin_product',{page_title:"Add products - Node.js"});
   };
-  
-  exports.edit = function(req, res){
-      
+
+  //Edit Product data  
+exports.edit = function(req, res){
       var id = req.params.id;
-      
       req.getConnection(function(err,connection){
-         
           var query = connection.query('SELECT * FROM products WHERE id = ?',[id],function(err,rows)
           {
-              
               if(err)
-                  console.log("Error Selecting : %s ",err );
-       
-              res.render('edit_admin_product',{page_title:"Edit products - Node.js",data:rows});
-                  
-             
+                console.log("Error Selecting : %s ",err );
+              res.render('edit_admin_product',{page_title:"Edit products - Node.js",data:rows});   
            });
-           
            //console.log(query.sql);
       }); 
   };
   
-  /*Save the product*/
+  /*Save the new product*/
   exports.save = function(req,res){
-      
       var input = JSON.parse(JSON.stringify(req.body));
-      
       req.getConnection(function (err, connection) {
-          
           var data = {
-              
               name    : input.name,
               description : input.description,
-              from_price   : input.price,
+             // from_price   : input.price,
               allergen_info   : input.allergen_info,
+              cat_id : input.cat_id,
               prd_img   : input.prd_img
- 
           };
-          
           var query = connection.query("INSERT INTO products set ? ",data, function(err, rows)
           {
-    
             if (err)
                 console.log("Error inserting : %s ",err );
-           
             res.redirect('/adminproducts');
-            
           });
           
          // console.log(query.sql); get raw query
       
       });
   };
-  
+  //save edit product admin
   exports.save_edit = function(req,res){
-      
       var input = JSON.parse(JSON.stringify(req.body));
       var id = req.params.id;
-      
       req.getConnection(function (err, connection) {
           
           var data = {
-              
             name    : input.name,
             description : input.description,
-            from_price   : input.price,
+            //from_price   : input.price,
             allergen_info   : input.allergen_info,
+            cat_id: input.cat_id,
             prd_img   : input.prd_img
           
           };
           
-          connection.query("UPDATE product set ? WHERE id = ? ",[data,id], function(err, rows)
+          connection.query("UPDATE products set ? WHERE id = ? ",[data,id], function(err, rows)
           {
     
             if (err)
