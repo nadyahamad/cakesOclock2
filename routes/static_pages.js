332c3e8867a0a2f6cc3142482a4012729d9f7a83
Nahamad <nahamad83@gmail.com>
2020-04-26 23:34:56 +0100

    about page

--- routes/static_pages.js
+++ routes/static_pages.js
@@ -1,11 +1,6 @@
 //Get about us page
 exports.about = function(req, res){
-	let user = req.session.user;
-    if(user) {
-        res.render('about_us', {opp:req.session.opp, name:user.fullname, data:user.id});
-        return;
-	}
-	res.render('/', {title: "About Us" });
+	res.render('about_us', {title: "About Us" });
 };
 
 //Get contact us page
